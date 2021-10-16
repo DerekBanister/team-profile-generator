9648a0bb75a3ec30ff7fc94c7d7c68a470550fb0
Derek Banister <ptownderek@yahoo.com>
2021-10-16 09:56:48 -0700

    employee test done, finish others then create constructors for each role

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -1,1 +1,39 @@
 // need to test for employee object, name, id ,email, role. 
+
+const employee = require("../lib/employee.js");
+
+test("create employee object", () => {
+
+    const employeeObj = new employee("Farley Chicken", "1", "bacon@chicken.net");
+    
+    //expect??
+    expect(employeeObj.name).toBe("Farley Chicken");
+    expect(employeeObj.id).toBe("1");
+    expect(employeeObj.email).toBe("bacon@chicken.net");
+});
+
+test("get employee name", () => {
+    const employeeObj = new employee("Farley Chicken", "1", "bacon@chicken.net");
+
+    expect(employee.getName()).toEqual(stringContaining("Farley Chicken"));
+})
+
+test("get employee ID", () => {
+    const employeeObj = new employee("Farley Chicken", "1", "bacon@chicken.net");
+
+    expect(employee.getId()).toEqual(stringContaining("1"));
+})
+
+test("get employee email", () => {
+    const employeeObj = new employee("Farley Chicken", "1", "bacon@chicken.net");
+
+    expect(employee.getEmail()).toEqual(stringContaining("bacon@chicken.net"));
+})
+
+test("get employee role", () => {
+    const employeeObj = new employee("Farley Chicken", "1", "bacon@chicken.net");
+
+    expect(employee.getRole()).toEqual(stringContaining("Employee"));
+})
+
+//not sure if these are correct, time will tell when I test later
